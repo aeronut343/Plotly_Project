600a419f56dbb390de5fc1e22924289256ccfc35
aeronut343 <halex211@umn.edu>
2021-09-24 18:58:07 -0500

    12.4.3

--- static/js/plot.js
+++ static/js/plot.js
@@ -4,3 +4,20 @@ d3.json("./static/resources/samples.json").then((data) => {
     Object.entries(firstPerson).forEach(([key, value]) =>
       {console.log(key + ': ' + value);});
 });
+
+// Initialize selections
+function init() {
+  let selector = d3.select("#selDataset");
+
+  d3.json("./static/resources/samples.json").then((data) => {
+    console.log(data);
+    let sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
