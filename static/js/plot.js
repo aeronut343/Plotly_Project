e94743fbe3cdf6bfd973a48bc187b71e56aac17c
aeronut343 <halex211@umn.edu>
2021-09-25 19:36:38 -0500

    finished pre challenge

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,9 +1,9 @@
 // Read in samples
-d3.json("./static/resources/samples.json").then((data) => {
-    firstPerson = data.metadata[0];
-    Object.entries(firstPerson).forEach(([key, value]) =>
-      {console.log(key + ': ' + value);});
-});
+// d3.json("./static/resources/samples.json").then((data) => {
+//     firstPerson = data.metadata[0];
+//     Object.entries(firstPerson).forEach(([key, value]) =>
+//       {console.log(key + ': ' + value);});
+// });
 
 // Initialize selections
 function init() {
@@ -21,3 +21,26 @@ function init() {
 })}
 
 init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("./static/resources/samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
+    PANEL.append("h6").text("GENDER: " + result.gender);
+    PANEL.append("h6").text("AGE: " + result.age);
+    PANEL.append("h6").text("LOCATION: " + result.location);
+    PANEL.append("h6").text("BBTYPE: " + result.bbtype);
+    PANEL.append("h6").text("WFREQ: " + result.wfreq);
+  });
+}
